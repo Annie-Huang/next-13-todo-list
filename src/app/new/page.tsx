9491b43bc888d592349b8ddb39b234d8526be6f8
Annie Huang <annie.hn@gmail.com>
2023-05-27 22:01:58 +1000

    Link the function as action in <form> and confirm 'Hi' will be printed in the terminal when click create button

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -14,7 +14,7 @@ export default function Page() {
       <header className='flex justify-between items-center mb-4'>
         <h1 className='text-2xl'>New</h1>
       </header>
-      <form className='flex gap-2 flex-col'>
+      <form action={createTodo} className='flex gap-2 flex-col'>
         <input
           type='text'
           name='title'
